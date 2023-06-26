8d116350db5f62fa63a3377210a6b950547e0359
methreban2023 <alrefai.dalia@gmail.com>
2023-06-26 12:58:11 +0300

    added error.message

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  return res.status(500).json({ message: "Internal Server Error" });
+  return res.status(500).json({ message: error.message });
 };
 
 module.exports = errorHandler;
